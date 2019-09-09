0f13cf5326d61e3c7f8a61979782ad00db5f2172
SpeedX <ggspeedx29@gmail.com>
2019-09-09 21:45:54 +0530

    some updates

--- torrgrab.py
+++ torrgrab.py
@@ -1,11 +1,14 @@
 import urllib.request,urllib.parse
 from bs4 import BeautifulSoup
 
+#PirateBay Alternate accessible link
+plnk="https://indiaboat.art"
+
 term=input("Enter What to search: ")
 term=urllib.parse.quote_plus(term.strip())
-site="https://indiaboat.art/s/?q="+term+"&page=&orderby="
-#print(site)
-#site="https://indiaboat.art/s/?q=Sacred+games&page=&orderby="
+site=plnk+"/s/?q="+term+"&page=&orderby="
+
+
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -26,4 +29,4 @@ for lnk in k:
 	lnk=str(lnk)
 	if lnk.find('class="detLink"')!=-1:
 		print(lnk[lnk.find('>')+1:lnk.find('</a')])
-#print(content)
+
